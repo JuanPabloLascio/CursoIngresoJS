46ecc782c15f194c88c2c4a3f7dc1858884a2d55
JuanPabloLascio <60608502+JuanPabloLascio@users.noreply.github.com>
2020-02-03 12:29:10 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,13 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+    //Creo una variable
+    var nombre;
+    //asigno a la variable nombre el valoe escrito en la caja de texto
+    // nombre es la variable / document.getElementById toma el documento dentro de la caja de texto / value hace la accion de comparar
+    nombre = document.getElementById("elNombre").value;
+    
+    alert("Su nombre es " + nombre);
 }
 
 
