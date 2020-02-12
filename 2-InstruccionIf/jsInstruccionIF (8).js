d41d57ca957201d5dd6d6cb9b2e6b55f267768a4
JuanPabloLascio <60608502+JuanPabloLascio@users.noreply.github.com>
2020-02-12 15:38:46 -0300

    12/2/2020
    Ejercicio 8

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -3,14 +3,21 @@ function mostrar()
 //tomo la edad  
 var edad
 var estado
+var select
 
 edad = document.getElementById("edad").value;
 estado = document.getElementById("estadoCivil").value;
 
 
+    if (edad > 17 ) {
+        
+        switch (estado) {
 
+            case "Soltero" :
+                alert("Es soltero y no es menor de edad");
+        }
+    }    
 
-	
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,5 +2,9 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	return Math.random(0, 10)
+	{
 
+		return Math.random() * (10 - 0) + 0;
+	}
 }//FIN DE LA FUNCIÓN
