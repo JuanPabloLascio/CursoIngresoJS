e5c42e0c186cf93628ba6bad9d1016a1b3e5fb78
JuanPabloLascio <60608502+JuanPabloLascio@users.noreply.github.com>
2020-02-14 09:44:34 -0300

    14/02/2020
    Terminado el if/else

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,23 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var numero;
+	var minimo = 1;
+	var maximo = 10;
+
+	numero = Math.floor(Math.random() * ((maximo + 1) - minimo) + minimo);
+
+	if (numero >= 9) {
+
+		alert("Tu nota es " + numero + ", EXELENTE.");
+	}
+	else if ( numero < 4) {
+
+		alert("Tu nota es " + numero + ", Vamos, la proxima se puede.");
+	}
+	else {
+
+		alert("Tu nota es " + numero + ", APROBÓ");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,9 +2,11 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
-	return Math.random(0, 10)
-	{
+	var numero;
+	var minimo = 1;
+	var maximo = 10;
 
-		return Math.random() * (10 - 0) + 0;
-	}
+	numero = Math.floor(Math.random() * ((maximo + 1) - minimo) + minimo);
+
+	alert(numero);
 }//FIN DE LA FUNCIÓN
