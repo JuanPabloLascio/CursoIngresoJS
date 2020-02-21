7dea0f596246f386107783b594ed0decf88c59fd
JuanPabloLascio <60608502+JuanPabloLascio@users.noreply.github.com>
2020-02-21 12:18:50 -0300

    21/02/2020
    21/02/2020

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,32 @@
 function mostrar()
-{
+{	
+	var flag;
+	var numero;
+	var respuesta = 's';
+	var positivo = 0;
+	var negativo = 0;
+	var cantp = 0;
+	var cantn;
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
+	do {
+		numero = parseInt(prompt("Ingrese un numero:"));
+		while (isNaN(numero)) {
+			numero = parseInt(prompt("Eso no es un numero. Ingrese un numero:"));
+		}
+		if ( numero > 0 || flag == 0) {
+			positivo = positivo + numero;
+			cantp++;
+		}
+		if ( numero < 0 || flag == 0) {
+			negativo = negativo + numero;
+			cantn++;
+		}
 
-	while(respuesta!="no")
-	{
 		
-	
-	}
+		respuesta = prompt("Quiere ingresar otro numero?");
+	} while ( respuesta == 's');
 
 
-
+	document.write()
 
 }//FIN DE LA FUNCIÓN
