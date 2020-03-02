371d5b2b2f3efd1cbdbbee34288153bb0f7fa1cc
JuanPabloLascio <60608502+JuanPabloLascio@users.noreply.github.com>
2020-03-02 12:19:52 -0300

    Hacer todo

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,91 @@
 function mostrar()
 {
+    var producto;
+    var peso;
+    var temperatura;
+    var seguir;
+    var temperaturaPares = 0;
+    var pesoMaspesado;
+    var productoMax;
+    var contadorProductosmenorgrados = 0;
+    var acumuladorPeso = 0;
+    var contadorProducto = 0;
+    var promedio;
+    var maximo;
+    var minimo;
+    var flag = 0;
+
+
+    //------ Pido datos mientras que el usuario quiera-------
+    do {
+        //------Producto------
+        producto = prompt("Marca del producto?");
+        //-----Peso del producto------
+        peso = parseInt(prompt("Cuanto pesa? (1 a 100): "));
+        while (isNaN(peso) || peso < 1 || peso > 100 ) {
+            peso = parseInt(prompt("Error, numero invalido. Cuanto pesa? (1 - 100): "));
+        }
+        //-------Temperatura del producto---------
+        temperatura = parseInt(prompt("Que temperatura tiene? (-30 a 30)"));
+        while ( isNaN(temperatura) || temperatura < -30 || temperatura > 30) {
+            temperatura = parseInt(prompt("Error, numero invalido. Que temperatura tiene? (-30 a 30)"));
+        }
+        // A ------------ Teperatura pares ----------
+        if (!(temperatura % 2 != 0)) {
+            temperaturaPares++;
+        }
+
+        // B ------ Marca mas pesada > 0 --------
+
+        if ( (flag == 0 || peso > pesoMaspesado) && temperatura > 0) {
+            productoMax = producto;
+            pesoMaspesado = peso;
+        }
+
+        // C -------- Cantidad de productos que se concerban a menos de 0 ------
+
+        if ( temperatura < 0) {
+            contadorProductosmenorgrados++;
+        }
+
+        // F El peso maximo y minimo
+        if ( flag == 0 || peso < minimo) {
+            minimo = peso;
+        }
+
+
+        if ( flag == 0 || peso > maximo) {
+            maximo = peso;
+            flag = 1;
+
+        }
+
+        // D -- Contador de peso y productos --
+
+        acumuladorPeso = acumuladorPeso + peso;
+        contadorProducto++;
+
+
+
+
+        seguir = prompt("Quieres continuar? ( s o n)");
+    }while (seguir == 's');
+
+    // ------- Promedio -------
+
+    promedio = acumuladorPeso / contadorProducto;
+
+    document.write("a) La cantidad de temperaturas pares " + temperaturaPares + "</br>");
+    document.write("b) La marca del producto más pesado que no sea congelado > 0. " + productoMax + " " + pesoMaspesado + "</br>");
+    document.write("c) La cantidad de productos que se conservan a menos de 0 grados. " + contadorProductosmenorgrados + "</br>");
+    document.write("d) El promedio del peso de todos los productos. " + promedio + "</br>");
+    document.write("El peso máximo " + maximo + " y el mínimo. " + minimo + "</br>");
+
+   /* Testeo ------
+   F / 10 / 15
+   A / 15 / -10
+   B / 20 / 1
+   C / 30 / -2S
+   */
 
 }
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,83 @@
 function mostrar()
 {
+    var seguir;
+    var pais;
+    var habitantes;
+    var temperatura;
+    var contadorTempPares = 0;
+    var menorHabitantes;
+    var paisMenorHabi;
+    var contadorPais = 0;
+    var promedio;
+    var acumuladorHabitantes = 0;
+    var contadorPaises = 0;
+    var minimo;
+    var paisMinimo;
+    var flag = 0;
+
+    do {
+
+        pais = prompt("Nombre del pais: ");
+
+        habitantes = parseInt(prompt("Cantidad de habitantes del pais (1 a 7000): "));
+        while ( habitantes < 1 || habitantes > 7000 || isNaN(habitantes)) {
+            habitantes = parseInt(prompt("Error, numero invalido. Cantidad de habitantes del pais: "));
+        }
+
+        temperatura = parseInt(prompt("Que temperatura hay? (-50 a 50)"));
+        while (temperatura < -50 || temperatura > 50 || isNaN(temperatura)) {
+            temperatura = parseInt(prompt("Error, numero invalido. Que temperatura hay? (-50 a 50)"))
+        }
+
+        // Cantidad de temperatura pares...
+
+        if ( temperatura % 2 == 0) {
+            contadorTempPares++;
+        }
+        // Nombre de pais con menos habitantes...
+
+        if ( flag == 0 || habitantes < menorHabitantes) {
+            menorHabitantes = habitantes;
+            paisMenorHabi = pais;
+        }
+
+        // Cantidad de paises que superan los 40º..
+
+        if ( temperatura > 40) {
+            contadorPais++;
+        }
+
+        // Promedio de habitantes entre los paises ingresados...
+
+        acumuladorHabitantes = acumuladorHabitantes + habitantes;
+        contadorPaises++;
+
+        // La temperatura minima ingreasa y el nombre del pais...
+
+        if ( flag == 0 || temperatura < minimo){
+            minimo = temperatura;
+            paisMinimo = pais;
+            flag == 1;
+
+        }
+
+        seguir = prompt("Quieres continuar? (s o n): ");
+    } while (seguir =='s');
+
+
+    promedio = acumuladorHabitantes / contadorPaises;
+
+    document.write("a) La cantidad de temperaturas pares : " + contadorTempPares + " </br>");
+    document.write("b) El nombre del pais con menos habitantes : " + paisMenorHabi + " </br>");
+    document.write("c) la cantidad de paises que superan los 40 grados. : " + contadorPais + " </br>");
+    document.write("d) el promedio de habitantes entre los paises ingresados : " + promedio + " </br>");
+    document.write("f) la temperatura mínima ingresada, y nombre del pais que registro esa temperatura. : " + "temperatura: " + minimo + ", Pais: " + paisMinimo + " </br>");
 
 }
+/*
+A / 200 / 35
+B / 100 / 50
+C / 500 / -15
+D /  50 / 41 
+ 
+*/
